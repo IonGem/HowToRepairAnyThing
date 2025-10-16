78f35ec3a2b8a64c3b9ca13c1f2bf2e8b28ed2c0
IonGem <daniil.michkov@mail.ru>
2025-10-16 18:05:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 alert('Тутор как починить все')
 let move = prompt('Это двигается?')
-if(move === 'да') {
+if(move === 'да' || move === 'Да') {
     let must = prompt('А должно?')
-    if(must === 'да') {
+    if(must === 'да' || must === 'Да') {
         alert('Не трогай')
-    } if(must === 'нет') {
+    } if(must === 'нет' || must === 'Нет') {
         alert('Изолента в помощь')
     }
 }
 
-if(move === 'нет') {
+if(move === 'нет' || move === 'Нет') {
     let must = prompt('А должно?')
-    if(must === 'да') {
+    if(must === 'да' || must === 'Да') {
         alert('wd-40 в помощь')
-    } if(must === 'нет') {
+    } if(must === 'нет' || must === 'Нет') {
         alert('Не трогай')
     }
+
 }
